e82f3b5540d80301beffdc66d3908e86a7ca33ae
Melowe <51891075+Mel-Lowe@users.noreply.github.com>
2022-03-07 18:36:31 +0000

    remove console.log

--- src/app.js
+++ src/app.js
@@ -2,12 +2,26 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let day = date.getDay();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#currentTempInteger");
   let cityElement = document.querySelector("#currentCity");
   let descriptionElement = document.querySelector("#current-description");
